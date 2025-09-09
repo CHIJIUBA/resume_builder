31f07664520fb461429626e305039b2c3ad8950e
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2025-09-09 14:09:33 +0100

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { TopBar } from './components/TopBar'
-import { SideNav } from './components/SideNav'
+import { TopBar } from './components/TopBar';
+import { SideNav } from './components/SideNav';
 
 function App() {
-
-
   return (
     <>
       <TopBar />
-      <div className={"maincontainer"}>
+      <div className={'maincontainer'}>
         <SideNav />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
